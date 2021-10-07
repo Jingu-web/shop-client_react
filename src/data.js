c02ccf4feeb36a361b44536ee501ef69e6408a3a
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-07 18:37:20 +0900

    ✨ スライダーの作成

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/RNFfz83/2.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/SBf3ZpM/Pa-HYQgg1-MHz-QDNG1633562820-1633562845.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/hBmrfYW/3.png",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Announcement } from "../components/Announcement";
 import { Header } from "../components/Header";
+import { Slider } from "../components/Slider";
 
 export const Home = () => {
   return (
     <div>
       <Announcement />
       <Header />
+      <Slider />
     </div>
   );
 };
