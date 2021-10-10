c5713b14011622a8012b2cc43ed4d7731641715f
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-10 16:12:00 +0900

    🎨 プロダクトリストページの作成

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+
+import { Header } from "../components/Header";
+import { Announcement } from "../components/Announcement";
+import { Products } from "../components/Products";
+import { Footer } from "../components/Footer";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+
+const Option = styled.option``;
+
+export const ProductList = () => {
+  return (
+    <Container>
+      <Header />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          {/* <FilterText>Fillter Products:</FilterText> */}
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          {/* <FilterText>並び替え:</FilterText> */}
+          <Select>
+            <Option selected>最新</Option>
+            <Option>価格の高い順</Option>
+            <Option>価格の安い順</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Footer />
+    </Container>
+  );
+};
